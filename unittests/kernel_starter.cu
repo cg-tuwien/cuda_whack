b7cd88eaff2f6546f44fa5162d4ca6b3f0aaa236
TheOwlforge <anna_accounts@protonmail.com>
2023-03-09 11:46:29 +0100

    adapted unit tests for windows (external linkage for lambdas)

--- unittests/kernel_starter.cu
+++ unittests/kernel_starter.cu
@@ -23,50 +23,64 @@
 
 #include "whack/kernel.h"
 
+// windows is only happy, if the enclosing function of a host device lambda has external linkage
+
+void kernel_starter_interface() {
+    dim3 dimBlock = dim3(1, 1, 1);
+    dim3 dimGrid = dim3(1, 1, 1);
+    whack::start_parallel(whack::ComputeDevice::CUDA, dimGrid, dimBlock, [] __host__ __device__(const dim3&, const dim3&, const dim3&, const dim3&) {});
+}
+
+void kernel_starter_start_on_cuda() {
+    thrust::device_vector<int> v(16);
+    int* v_ptr = thrust::raw_pointer_cast(v.data());
+    dim3 dimBlock = dim3(32, 1, 1);
+    dim3 dimGrid = dim3(1, 1, 1);
+    whack::start_parallel(whack::ComputeDevice::CUDA, dimGrid, dimBlock, [v_ptr] __host__ __device__(const dim3 & gpe_gridDim, const dim3 & gpe_blockDim, const dim3 & gpe_blockIdx, const dim3 & gpe_threadIdx) {
+        if (gpe_threadIdx.x >= 16)
+            return;
+        v_ptr[gpe_threadIdx.x] = gpe_threadIdx.x;
+    });
+
+    thrust::host_vector<int> host_v(v);
+    REQUIRE(host_v.size() == 16);
+    for (int i = 0; i < 16; ++i) {
+        CHECK(host_v[i] == i);
+    }
+}
+
+void kernel_starter_start_on_cpu() {
+    thrust::host_vector<int> v(16);
+    int* v_ptr = thrust::raw_pointer_cast(v.data());
+    dim3 dimBlock = dim3(32, 1, 1);
+    dim3 dimGrid = dim3(1, 1, 1);
+    whack::start_parallel(whack::ComputeDevice::CPU, dimGrid, dimBlock, [v_ptr] __host__ __device__(const dim3 & gpe_gridDim, const dim3 & gpe_blockDim, const dim3 & gpe_blockIdx, const dim3 & gpe_threadIdx) {
+        if (gpe_threadIdx.x >= 16)
+            return;
+        v_ptr[gpe_threadIdx.x] = gpe_threadIdx.x;
+    });
+
+    REQUIRE(v.size() == 16);
+    for (int i = 0; i < 16; ++i) {
+        CHECK(v[i] == i);
+    }
+}
+
 TEST_CASE("kernel_starter: start cuda kernel")
 {
 
     SECTION("interface")
     {
-        dim3 dimBlock = dim3(1, 1, 1);
-        dim3 dimGrid = dim3(1, 1, 1);
-        whack::start_parallel(whack::ComputeDevice::CUDA, dimGrid, dimBlock, [] __host__ __device__(const dim3&, const dim3&, const dim3&, const dim3&) {});
+        kernel_starter_interface();
     }
 
     SECTION("start on cuda")
     {
-        thrust::device_vector<int> v(16);
-        int* v_ptr = thrust::raw_pointer_cast(v.data());
-        dim3 dimBlock = dim3(32, 1, 1);
-        dim3 dimGrid = dim3(1, 1, 1);
-        whack::start_parallel(whack::ComputeDevice::CUDA, dimGrid, dimBlock, [v_ptr] __host__ __device__(const dim3& gpe_gridDim, const dim3& gpe_blockDim, const dim3& gpe_blockIdx, const dim3& gpe_threadIdx) {
-            if (gpe_threadIdx.x >= 16)
-                return;
-            v_ptr[gpe_threadIdx.x] = gpe_threadIdx.x;
-        });
-
-        thrust::host_vector<int> host_v(v);
-        REQUIRE(host_v.size() == 16);
-        for (int i = 0; i < 16; ++i) {
-            CHECK(host_v[i] == i);
-        }
+        kernel_starter_start_on_cuda();
     }
 
     SECTION("start on cpu")
     {
-        thrust::host_vector<int> v(16);
-        int* v_ptr = thrust::raw_pointer_cast(v.data());
-        dim3 dimBlock = dim3(32, 1, 1);
-        dim3 dimGrid = dim3(1, 1, 1);
-        whack::start_parallel(whack::ComputeDevice::CPU, dimGrid, dimBlock, [v_ptr] __host__ __device__(const dim3& gpe_gridDim, const dim3& gpe_blockDim, const dim3& gpe_blockIdx, const dim3& gpe_threadIdx) {
-            if (gpe_threadIdx.x >= 16)
-                return;
-            v_ptr[gpe_threadIdx.x] = gpe_threadIdx.x;
-        });
-
-        REQUIRE(v.size() == 16);
-        for (int i = 0; i < 16; ++i) {
-            CHECK(v[i] == i);
-        }
+        kernel_starter_start_on_cpu();
     }
 }
